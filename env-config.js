0fed57d92d7a676f392ec16e233dec53d68920eb
Luca Santarella <luca.santarella@gmail.com>
2022-06-09 08:24:21 -0400

    Updated mount path
    Former-commit-id: 4180b0169af20f86caf1bee8b9be92190f546c2b

--- env-config.js
+++ env-config.js
@@ -1,6 +1,7 @@
 const prod = process.env.NODE_ENV === 'production'
+const mountUrl = process.env.MOUNT_URL ? process.env.MOUNT_URL : '/website-gh'
 
 module.exports = {
-  'process.env.BACKEND_URL': prod ? '/website-gh' : '',
+  'process.env.MOUNT_URL': mountUrl ? '/website-gh' : '',
   'process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS': 'UA-43970239-1',
 }
