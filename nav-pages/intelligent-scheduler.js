86b4a98c75789baa57bf533cffbb495993013833
ovo <ovoe@decagonhq.com>
2023-03-02 17:41:04 +0100

    intelligent scheduler changes
    Former-commit-id: 505417fd1f50d150a1db4dabb27e3f2863bc7d70

---
+++ nav-pages/intelligent-scheduler.js
@@ -0,0 +1,56 @@
+import React, { useContext } from "react";
+import { Context, changePage } from "../context/context";
+import Head from "next/head";
+import HeaderNav from "../components/HeaderNav";
+import NumberScroll from "../components/NumberScroll";
+import FooterBar from "../components/Footer";
+import { useMedia } from "../hooks/useMedia";
+import AnnoucementBanner from "../components/AnnoucementBanner";
+
+export default function IntelligentScheduler() {
+  const { state, dispatch } = useContext(Context);
+  const isBrowser = () => typeof window !== "undefined";
+  let smallScreen;
+  if (isBrowser) {
+    smallScreen = useMedia("(max-width: 1100px)");
+  }
+
+  return (
+    <>
+      <AnnoucementBanner />
+      <div className="container">
+        <Head>
+          <title>
+            DocSpera | HIPAA Compliant, Integrated Surgical Coordination
+            Platform
+          </title>
+          <link
+            rel="icon"
+            href="https://assets.d4.docspera.com/home/favicon.ico"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
+
+        <HeaderNav />
+        <div className='container'>
+
+
+          
+        </div>
+       
+
+        <FooterBar />
+        <style jsx>{`
+          .container {
+            width: 100%;
+          }
+        `}</style>
+      </div>
+    </>
+  );
+}
