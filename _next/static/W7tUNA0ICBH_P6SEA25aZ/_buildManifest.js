748232a39854d0ed5701a9ee5c4d03ae536964ca
lucasantarella <lucasantarella@users.noreply.github.com>
2023-03-15 13:36:00 +0000

    Deploying to gh-pages from @ CompliantInnovation/website-gh@cd9bcea69619624429e9ea2ec1d1bfa6430c2fc7 🚀

---
+++ _next/static/W7tUNA0ICBH_P6SEA25aZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/491-1e0582af20849c8fe7ca.js","static/chunks/pages/index-ee246c8081ac81271811.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
